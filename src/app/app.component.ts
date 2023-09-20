c6811eb72f2327122b8714d92f6dc827f1618e5f
Hadi <1774020+hadideveloper@users.noreply.github.com>
2023-09-20 18:46:21 +0800

    Connect and Read Trezor device ID

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import * as core from "@shapeshiftoss/hdwallet-core";
+import * as trezorConnect from "@shapeshiftoss/hdwallet-trezor-connect";
+
+const keyring = new core.Keyring();
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,11 +12,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  //! view variables
   title = 'test-shapeshift-hdwallet';
+  deviceId = '';
 
-  btnConnectTrezor(){
+  //! wallet
+  private _wallet:any;
+
+  //! wallet adapters
+  private readonly _trezorAdapter = trezorConnect.TrezorAdapter.useKeyring(keyring, {
+    debug: false,
+    manifest: {
+      email: "oss@shapeshiftoss.io",
+      appUrl: "https://shapeshift.com",
+    },
+  });
+
+  async btnConnectTrezor(){
     console.log("Connecting to Trezor");
-    
+    this._wallet = await this._trezorAdapter.pairDevice();
+
+    this.deviceId = await this._wallet.getDeviceID();
+
   }
 
   btnConnectLedger(){
